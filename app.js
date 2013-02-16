3764c1d8ef4c640e34a9bda424a8ca1e8679c0d8
glassc <glassc@gmail.com>
2013-02-15 20:25:24 -0500

    Moved admin to module

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 var express = require('express');
 var app = express();
-var admin = require('./controllers/admin');
 
 var authentication = require('./lib/authentication');
 
-app.set('views', __dirname + '/views');
+
 app.engine('ejs', require('ejs-locals'));
 app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.bodyParser());
 app.use(express.cookieParser());
 app.use(express.session({secret: "la la la"}));
-app.use(express.static('./content/', { maxAge: 1 }));
+
 
 //authentication.enable(app);
-
+require("./admin")(app);
 require("./pages")(app);
 require('./users')(app);
 require("./images")(app);
@@ -26,9 +25,7 @@ require("./images")(app);
 
 
 // ADMIN
-app.get("/admin/login", admin.login);
-//app.all("/admin/*", authentication.required);
-app.get("/admin", admin.index);
+
 
 
 app.listen(process.env.PORT);
--- controllers/admin.js
+++
@@ -1,7 +0,0 @@
-exports.index = function(req, res) {
-    res.render('admin/index');
-}
-
-exports.login = function(req,res) {
-    res.render('admin/login');
-}
