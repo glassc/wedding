b8bba8c00a72b18e4780521b2f191c0b7f4458cd
glassc <glassc@gmail.com>
2013-03-27 17:25:49 -0400

    Gallery images can now have captions

--- pages/view/gallery.viewmodel.js
+++ pages/view/gallery.viewmodel.js
@@ -10,6 +10,8 @@ define(["knockout", "jquery", "kosortable"], function(ko, $) {
         self.selected = ko.observable(false);
         self.images = ko.observableArray();
         self.type = "Gallery";
+        self.currentImage = null;
+        
         
         if( images )
         {
@@ -44,24 +46,24 @@ define(["knockout", "jquery", "kosortable"], function(ko, $) {
                 });    
         }
         
-        function AddImage(url)
+        function AddImage(image)
         {
-            self.images.push(new ImageViewModel(url));
+            self.images.push(new ImageViewModel(image));
         }
         
         self.ToJSON = function()
         {
-            var urls = [];
+            var imageArr = [];
             
             self.images().forEach(function(image) {
-               urls.push(image.image); 
+               imageArr.push({url: image.image, caption: image.caption}); 
             });
             
             
             
             return {
                 type: "gallery",
-                images: urls
+                images: imageArr
             }
           
             
@@ -78,19 +80,48 @@ define(["knockout", "jquery", "kosortable"], function(ko, $) {
             
             self.images.remove(image);
         }
+        
+        self.Edit = function(image)
+        {
+            if( InEditMode() ) return;
+            image.ToggleEditMode(true);
+            self.currentImage = image;
+        }
+        
+        self.GalleryMode = function(image)
+        {
+            image.ToggleEditMode(false);
+            self.currentImage = null;
+        }
+        
+        function InEditMode()
+        {
+            return self.currentImage !== null;
+        }
+       
     }
     
-    function ImageViewModel(url)
+    function ImageViewModel(image)
     {
         var self = this;
-        self.image = ko.observable(url);
-        
+        self.image = ko.observable(image.url);
+        self.View = ko.observable("gallery-content-template-small");
         self.selected = ko.observable(false);
+        self.caption = ko.observable(image.caption);
         
         self.ToggleSelected = function()
         {
              self.selected( !self.selected() );
         }
+        
+        self.ToggleEditMode = function(inEditMode)
+        {
+            self.View(inEditMode ? "gallery-content-template-edit" : "gallery-content-template-small")
+        }
+        
+       
+        
+        
     }
     
     
