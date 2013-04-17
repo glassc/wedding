dffbb6b1ea23447c71121b5a2e4c17487d14ff36
glassc <glassc@gmail.com>
2013-04-17 18:49:03 -0400

    added tests to build script

--- gruntfile.js
+++ gruntfile.js
@@ -5,11 +5,18 @@ module.exports = function(grunt)
         
         jshint: {
             files: ['gruntfile.js', 'app.js','images/**/*.js', 'lib/*.js', 'pages/*.js', 'users/*.js', 'tests/**/*.js']
-        }
+        },
+        cafemocha: {
+             src: 'tests/**/*_test.js',
+             options: {
+                ui: 'bdd',
+             },
+  }
        
     });
     
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-cafe-mocha');
     
-    grunt.registerTask('default', 'jshint');
+    grunt.registerTask('default', 'jshint', 'cafemocha');
 };
