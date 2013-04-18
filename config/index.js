d55bdd6751d622502d9c26922265a221de38f315
glassc <glassc@gmail.com>
2013-04-17 23:11:03 -0400

    Added deploy task

--- config/index.js
+++ config/index.js
@@ -1,3 +1,1 @@
-var env = process.env.NODE_ENV == null ? "dev" : "production";
-
-module.exports = require("./" + env +  ".config.js");
+module.exports = require("./config.js");
--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,5 @@
+
+
 module.exports = function(grunt)
 {
     grunt.initConfig({
@@ -11,12 +13,49 @@ module.exports = function(grunt)
              options: {
                 ui: 'bdd',
              },
-  }
-       
+        },
+        copy:  {
+            deployment_files : {
+         
+                files : [
+                    {expand: true, src: ['admin/**'], dest: 'release/'},
+                    {expand: true, src: ['components/**'], dest: 'release/'},
+                    {expand: true, src: ['images/**'], dest: 'release/'},
+                    {expand: true, src: ['lib/**'], dest: 'release/'},
+                    {expand: true, src: ['pages/**'], dest: 'release/'},
+                    {expand: true, src: ['users/**'], dest: 'release/'},
+                    {expand: true, src: ['app.js'], dest: 'release/'},
+                    {expand: true, src: ['package.json'], dest: 'release/'},
+                    {expand: true, src: ['config/production.config.js'], dest: 'release/config/', 
+                        rename: function(dest, src) {
+                            return dest +  'config.js';
+                        }
+                    
+                    },
+                ]
+            }
+            
+        },
+        clean: {
+            release: ["release/"]
+        },
+        exec: {
+          modulus: {
+              stdout: false,
+              cmd: 'modulus deploy  -p wedding release'
+          }  
+        }
+        
+          
     });
     
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-cafe-mocha');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-exec');
     
     grunt.registerTask('default', 'jshint', 'cafemocha');
+    grunt.registerTask('deploy', 'clean:release', 'copy:deployment_files');
+
 };
