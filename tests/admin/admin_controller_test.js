6c4ae7a9935d4eb387275557980211a5a4807ace
glassc <glassc@gmail.com>
2013-04-22 23:05:12 -0400

    Added index test in admin_controller

--- tests/admin/admin_controller_test.js
+++ tests/admin/admin_controller_test.js
@@ -1,16 +1,19 @@
 var assert = require("assert");
 var sinon = require("sinon");
 var express = require("express");
+var db = require("../../lib/connection");
 
 describe("admin_controller", function() {
     var controller = null;
-    var db = null;
     var response = null;
     var sandbox = null;
     
     beforeEach(function() {
-        sandbox = sinon.sandbox.create()
-        db = sandbox.stub(require("../../lib/connection"));
+        sandbox = sinon.sandbox.create();
+        
+        
+        
+        
         controller = require("../../admin/admin_controller")(db);
         response = {render: sandbox.spy()}
     
@@ -25,6 +28,22 @@ describe("admin_controller", function() {
         var request = {flash: function() {}};
         sandbox.stub({flash: function() {}}, "flash").returns("");
         controller.login(request, response);
+        assert(response.render.calledOnce);
+    });
+    
+    it('should get the index page', function(done) {
+        var collection =  {
+            find:  function() {
+                return {
+                    toArray: function(callback) {
+                        callback(null, []);
+                        done();
+                    }
+                }
+            }
+        }
+        sandbox.stub(db, "collection").returns(collection)
+        controller.index(null, response);
         assert(response.render.calledOnce);
     });
 });
